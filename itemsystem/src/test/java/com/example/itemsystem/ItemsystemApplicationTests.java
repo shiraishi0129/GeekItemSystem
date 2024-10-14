a30a17452b2965d9d50e6554fa6461215baa220f
Yuki Shiraishi <y.shiraishi.cingroup2012@gmail.com>
2024-10-15 08:50:40 +0900

    初期設定内容

---
+++ itemsystem/src/test/java/com/example/itemsystem/ItemsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itemsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
